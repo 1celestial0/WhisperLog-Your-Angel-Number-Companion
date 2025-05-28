87f1f3951b8ed0f898471f33c7ca575a4edee5b4
sourav dutta <sourav.007.dutta@gmail.com>
2025-05-28 02:47:29 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: In HTML, whitespace text nodes cannot be a child of <html>. Make sure you don't have any extra whitespace between tags on each line of your source code.
    > This will cause a hydration error.. Error source: See more info here: https://nextjs.org/docs/messages/react-hydration-error
    >
    > ...
    >     <HotReload assetPrefix="" globalError={[...]}>
    >       <AppDevOverlay state={{nextId:1, ...}} globalError={[...]}>
    >         <AppDevOverlayErrorBoundary globalError={[...]} onError={function bound dispatchSetState}>
    >           <ReplaySsrOnlyErrors>
    >           <DevRootHTTPAccessFallbackBoundary>
    >             <HTTPAccessFallbackBoundary notFound={<NotAllowedRootHTTPFallbackError>}>
    >               <HTTPAccessFallbackErrorBoundary pathname="/analytics" notFound={<NotAllowedRootHTTPFallbackError>} ...>
    >                 <RedirectBoundary>
    >                   <RedirectErrorBoundary router={{...}}>
    >                     <Head>
    >                     <link>
    >                     <script>
    >                     <script>
    >                     <script>
    >                     <RootLayout>
    > >                     <html lang="en" className="dark">
    > >                       {" "}
    >                         ...
    >                     ...
    >         ...
    >
    >
    > src/app/layout.tsx (35:5) @ RootLayout
    >
    >   33 | }>) {
    >   34 |   return (
    > > 35 |     <html lang="en" className="dark"> {/* Force dark theme based on globals.css */}
    >      |     ^
    >   36 |       <body className={`${geistSans.variable} ${geistMono.variable} ${caveat.variable} antialiased`}>
    >   37 |         <Providers>
    >   38 |           <main className="min-h-screen flex flex-col">
    >
    > Call Stack
    > 14
    >
    > Show 12 ignore-listed frame(s)
    > html
    > <anonymous> (0:0)
    > RootLayout
    > src/app/layout.tsx (35:5)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark"> {/* Force dark theme based on globals.css */}
+    <html lang="en" className="dark">
       <body className={`${geistSans.variable} ${geistMono.variable} ${caveat.variable} antialiased`}>
         <Providers>
           <main className="min-h-screen flex flex-col">
